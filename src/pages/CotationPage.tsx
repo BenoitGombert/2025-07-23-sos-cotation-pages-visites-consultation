1b9b648df053090af302436bdf5435764eba230a
Benoit Gombert <benoitgombert@MacBook-Air-de-Benoit.local>
2025-07-24 15:09:00 +0200

    Supprime import React non utilisé

--- src/pages/CotationPage.tsx
+++ src/pages/CotationPage.tsx
@@ -1,4 +1,5 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react'; // import { useState, useEffect } from 'react'; // React non requis avec Vite
+
 import styles from '../components/Button.module.css'
 
 // Juste avant getActesEtMajorations
