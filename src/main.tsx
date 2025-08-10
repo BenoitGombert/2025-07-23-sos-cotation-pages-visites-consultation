cb36d940f9e23b3b5aa7206a68282cf752aa7d38
Benoit Gombert <benoitgombert@MacBook-Air-de-Benoit.local>
2025-08-10 22:46:05 +0200

    Correction de l'import .tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,6 @@
-// src/main.tsx
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.tsx';
+import App from './App';
 import { registerSW } from 'virtual:pwa-register';
 
 registerSW({
